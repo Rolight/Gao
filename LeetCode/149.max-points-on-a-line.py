d461f63d7b3c21426e3e7de41a4ed65256a370b6
rolight <flsnnx@gmail.com>
2018-04-09 13:51:46 +0800

    leetcode 149

--- LeetCode/149.max-points-on-a-line.py
+++ LeetCode/149.max-points-on-a-line.py
@@ -1,138 +1,60 @@
-/*
- * [149] Max Points on a Line
- *
- * https://leetcode.com/problems/max-points-on-a-line/description/
- *
- * algorithms
- * Hard (15.19%)
- * Total Accepted:    91.9K
- * Total Submissions: 605.3K
- * Testcase Example:  '[]'
- *
- * Given n points on a 2D plane, find the maximum number of points that lie on
- * the same straight line.
- */
-/**
- * Definition for a point.
- * class Point {
- *     int x;
- *     int y;
- *     Point() { x = 0; y = 0; }
- *     Point(int a, int b) { x = a; y = b; }
- * }
- */
-import java.util.*;
+# Definition for a point.
+# class Point:
+#     def __init__(self, a=0, b=0):
+#         self.x = a
+#         self.y = b
+from collections import defaultdict
+from math import sqrt
 
-import static java.lang.Math.*;
 
-/*
-x1, x2, y1, y2
-y = k * x + b
-b = y1 - k * x1
-k = (y1 - y2) / (x1 - x2)
-y = (y1 - y2) * x / (x1 - x2) + y1 - (y1 - y2) * x1 / (x1 - x2)
-(x1 - x2) * y = (y1 - y2) * x + y1 * (x1 - x2) - (y1 - y2) * x1
-(y1 - y2) * x - (x1 - x2) *y + y1 * (x1 - x2) - (y1 - y2) * x1 = 0
-A * x + B * y + C = 0
-A = y1 - y2
-B = - (x1 - x2)
-C = y1 * (x1 - x2) - (y1 - y2) * x1
-*/
+class Solution:
+    def gcd(self, a, b):
+        if b == 0:
+            return a
+        return self.gcd(b, a % b)
 
-/*
-C(N, 2) = N * (N - 1) / 2 = V
-N ^ 2 - N - 2 * V = 0
-delta = 1 + 8 * V
-N = (1 + sqrt(1 + 8 * V)) / 2
-*/
+    def maxPoints(self, points):
+        """
+        :type points: List[Point]
+        :rtype: int
+        """
+        pcount = defaultdict(int)
+        upoints = []
+        for p in points:
+            if pcount[(p.x, p.y)] == 0:
+                upoints.append((p.x, p.y))
+            pcount[(p.x, p.y)] += 1
 
-class Solution {
-    public int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
-    }
-    public int gcd(int a, int b, int c) {
-        return gcd(abs(a), gcd(abs(b), abs(c)));
-    }
-    public int maxPoints(Point[] points) {
-        HashMap<Integer, HashMap<Integer, HashMap<Integer, Integer> > > hasht = new HashMap<>();
-        HashMap<Integer, HashMap<Integer, HashMap<Integer, HashMap<Integer, Boolean> > > > vis =
-            new HashMap<>();
-        HashMap<Integer, HashMap<Integer, Integer> > pcount = new HashMap<>();
-        List<Point> up = new ArrayList<Point>();
+        vis = set()
+        count = defaultdict(int)
 
-        for (int i = 0; i < points.length; i++) {
-            int x = points[i].x, y = points[i].y;
-            if (pcount.get(x) == null) {
-                pcount.put(x, new HashMap<>());
-            }
-            if (pcount.get(x).get(y) == null) {
-                up.add(points[i]);
-                pcount.get(x).put(y, 1);
-            } else {
-                int val = pcount.get(x).get(y);
-                pcount.get(x).put(y, val + 1);
-            }
-        }
+        def agg(x): return x * (x - 1) // 2
+        ans = min(1, len(points))
+        for i in range(len(upoints)):
+            ans = max(ans, pcount[upoints[i]])
+            for j in range(i + 1, len(upoints)):
+                x1, y1 = upoints[i]
+                x2, y2 = upoints[j]
+                A = y1 - y2
+                B = -(x1 - x2)
+                C = y1 * (x1 - x2) - (y1 - y2) * x1
 
-        int ans = min(1, points.length);
-        for (int i = 0; i < up.size(); i++) {
-            int x = up.get(i).x, y = up.get(i).y;
-            int cur_count = pcount.get(x).get(y);
-            ans = max(ans, cur_count);
-            for (int j = i + 1; j < up.size(); j++) {
-                int x1 = up.get(i).x, x2 = up.get(j).x;
-                int y1 = up.get(i).y, y2 = up.get(j).y;
-                int A = y1 - y2;
-                int B = -(x1 - x2);
-                int C = y1 * (x1 - x2) - (y1 - y2) * x1;
-                int[] arr = {A, B, C};
-                int g = 1;
-                for (int k = 0; k < 3; k++)
-                    if (arr[k] != 0)
-                        g *= arr[k];
-                for (int k = 0; k < 3; k++) {
-                    if (arr[k] != 0) {
-                        g = gcd(g, arr[k]);
-                    }
-                }
-                A /= g;
-                B /= g;
-                C /= g;
-                int val = 0, count1 = pcount.get(x1).get(y1), count2 = pcount.get(x2).get(y2);
-                if (hasht.get(A) == null) {
-                    hasht.put(A, new HashMap<>());
-                    vis.put(A, new HashMap<>());
-                }
-                if (hasht.get(A).get(B) == null) {
-                    hasht.get(A).put(B, new HashMap<>());
-                    vis.get(A).put(B, new HashMap<>());
-                }
-                if (vis.get(A).get(B).get(C) == null) {
-                    vis.get(A).get(B).put(C, new HashMap<>());
-                }
-
-                if (vis.get(A).get(B).get(C).get(i) == null) {
-                    vis.get(A).get(B).get(C).put(i, true);
-                    val += (count1 - 1) * count1 / 2;
-                }
-                if (vis.get(A).get(B).get(C).get(j) == null) {
-                    vis.get(A).get(B).get(C).put(j, true);
-                    val += (count2 - 1) * count2 / 2;
-                }
-                val += count1 * count2;
-                if (hasht.get(A).get(B).get(C) == null) {
-                    val += hasht.get(A).get(B).get(C);
-                }
-                System.out.printf("(%dx+%dy+%d=0), count=%d\n", A, B, C, val);
-                hasht.get(A).get(B).put(C, val);
-                int delta = 1 + 8 * val;
-                int sqrt_delta = (int) sqrt(delta);
-                if (delta == sqrt_delta * sqrt_delta && (1 + sqrt_delta) % 2 == 0) {
-                    int count = (1 + sqrt_delta) / 2;
-                    ans = max(ans, count);
-                }
-            }
-        }
-        return ans;
-    }
-}
+                g = 1
+                for v in [abs(v) for v in [A, B, C] if v != 0]:
+                    g *= v
+                for v in [abs(v) for v in [A, B, C] if v != 0]:
+                    g = self.gcd(g, v)
+                if (-A // g, -B // g, -C // g) in count:
+                    g *= -1
+                A, B, C = A // g, B // g, C // g
+                # print('cur A, B, C = ', (A, B, C))
+                count[(A, B, C)] += pcount[upoints[i]] * pcount[upoints[j]]
+                if (A, B, C, i) not in vis:
+                    count[(A, B, C)] += agg(pcount[upoints[i]])
+                    vis.add((A, B, C, i))
+                if (A, B, C, j) not in vis:
+                    count[(A, B, C)] += agg(pcount[upoints[j]])
+                    vis.add((A, B, C, j))
+        for k, v in count.items():
+            ans = max(ans, (1 + sqrt(1 + 8 * v)) // 2)
+        return int(ans)
