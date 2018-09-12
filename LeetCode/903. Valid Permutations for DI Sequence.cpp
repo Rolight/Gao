980e5a753cdcd760d5137b21925510c073a7b9b2
rolight <flsnnx@gmail.com>
2018-09-12 09:53:05 +0800

    hotfix leetcode 903

--- LeetCode/903. Valid Permutations for DI Sequence.cpp
+++ LeetCode/903. Valid Permutations for DI Sequence.cpp
@@ -9,36 +9,8 @@ const LL mod = 1e9 + 7;
 const int maxn = 205;
 LL f[maxn][maxn];
 class Solution {
-    vector<int> peak;
-    void get_peak(string S) {
-        peak.clear();
-        if(S[0] == 'D') peak.push_back(1);
-        for(int i = 1; i < S.length(); i++) {
-            peak.push_back((int)(S[i - 1] == 'I' && S[i] == 'D'));
-        }
-        if(S.back() == 'I') peak.push_back(1);
-        else peak.push_back(0);
-        
-        cout << "peak: ";
-        for(int i = 0; i < peak.size(); i++) cout << peak[i] << " ";
-        cout << endl;
-    }
 public:
     int numPermsDISequence(string S) {
-        /*
-        get_peak(S);
-        vector<LL> f;
-        f.push_back(1);
-        int peak_sum = peak[0];
-        for(int i = 1; i < peak.size(); i++) {
-            peak_sum += peak[i];
-            f.push_back(f.back() * peak_sum);
-            if(S[i - 1] == 'D' && i > 1) {
-                f.back() = f.back() + 1;
-            }
-        }
-        return f.back();
-        */
         memset(f, 0, sizeof(f));
         f[0][0] = 1;
         for(int i = 1; i <= S.size(); i++) {
